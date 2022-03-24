af0d1bfd36fc6cb579882b104a3ef96e8bcf0e5e
Yinshangyi <jl.dejaeger@gmail.com>
2022-03-24 17:52:51 +0100

    requirements.txt and update readme and main.py

--- README.md
+++ README.md
@@ -8,3 +8,26 @@ In order to pull a remote branch that you don't have locally, please do the foll
 git fetch --all
 git checkout myBranch
 ```
+
+#### Set up virtual environment
+
+**Create virtual environment** 
+```
+python3 -m venv env
+```
+**Activate the virtual environment**
+```
+source env/bin/activate
+```
+**Install Python libraries**
+```
+pip install -r requirements.txt
+```
+**Run script**
+```
+python main.py
+```
+**Deactivate the virtual environment**
+```
+deactivate
+```
--- intro_webservices/python_review/dejaeger/main.py
+++ intro_webservices/python_review/dejaeger/main.py
@@ -1,1 +1,23 @@
 # Solution
+import json
+
+# Step 1 Read JSON file
+with open("../data.json", "r") as file:
+    json_data = file.read()
+
+# Step 2 Parse JSON
+data = json.loads(json_data)
+
+# Step 3 Extract JSON data into a list of dict
+clean_data = []
+for people in data:
+    people_data = {
+        "firstname": people["info"]["firstname"],
+        "lastname": people["info"]["lastname"],
+        "address": people["info"]["address"]["street"] + people["info"]["address"]["city"] + people["info"]["address"]["zipcode"],
+        "age": people["info"]["age"],
+        "job": people["job"]
+    }
+    clean_data.append(people_data)
+
+print(clean_data)
